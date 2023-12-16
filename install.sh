ccb2437bb8aaf617546d944cfc1aa5b5288996a3
blakeaw <blakeaw1102@gmail.com>
2023-12-15 20:12:32 -0600

    Simple install script.

---
+++ install.sh
@@ -0,0 +1,9 @@
+# NOTE: requires sudo
+# To install use:
+# sudo bash install.sh
+
+# Copy the chext script to 
+# /usr/local/bin and make 
+# it executable. 
+cp ./chext /usr/local/bin/
+chmod u+x /usr/local/bin/chext
